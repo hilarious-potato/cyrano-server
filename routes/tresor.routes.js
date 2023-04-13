d5abad5e619467e67176f300496fc332f314b246
denefi <denefi@posteo.de>
2023-04-13 17:17:52 +0200

    handle user update on tresor create or delete

--- routes/tresor.routes.js
+++ routes/tresor.routes.js
@@ -3,26 +3,47 @@ const { Types } = require("mongoose");
 const router = express.Router();
 const Tresor = require("../models/Tresor.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
+const User = require("../models/User.model");
 
 // create
 
-router.post("/", (req, res, next) => {
-  const data = ({ title, messages } = req.body);
+router.post("/", isAuthenticated, (req, res, next) => {
+  const { title, messages } = req.body;
+  const data = { title, messages };
+  let tresor = null;
   //   console.log("data: ", data);
-
-  Tresor.create(data)
-    .then((response) => {
-      //   console.log("response: ", response);
-      res.status(201).json(response);
-    })
-    .catch((err) => {
-      //   console.log("err: ", err);
-      res.status(500).json(err);
-      next(err);
-    });
+  if (!(typeof data.title === "string" && data.title.length > 0)) {
+    res.status(400).json({ message: "Please provide a Tresor name" });
+  } else if (!(typeof data.messages === "string")) {
+    res
+      .status(400)
+      .json({ message: "Please provide a Tresor messages string" });
+  } else {
+    Tresor.create(data)
+      .then((response) => {
+        //   console.log("response: ", response);
+        tresor = response;
+        return User.findByIdAndUpdate(
+          req.payload._id,
+          {
+            $push: { tresors: response._id },
+          },
+          { new: true }
+        );
+      })
+      .then((response) => {
+        res.json({
+          message: `tresor created successfully for ${response.name}`,
+          tresor,
+        });
+      })
+      .catch((err) => {
+        //   console.log("err: ", err);
+        res.status(500).json(err);
+        next(err);
+      });
+  }
 });
-
-// read
 
 router.get("/:tresorId", isAuthenticated, (req, res, next) => {
   const { tresorId } = req.params;
@@ -33,7 +54,13 @@ router.get("/:tresorId", isAuthenticated, (req, res, next) => {
     Tresor.findById(tresorId)
       .then((response) => {
         // console.log("response: ", response);
-        res.status(200).json(response);
+        if (response) {
+          res.status(200).json(response);
+        } else {
+          res.status(404).json({
+            message: `Tresor with ID: ${tresorId} was not found on server`,
+          });
+        }
       })
       .catch((err) => {
         // console.log("err: ", err);
@@ -44,9 +71,10 @@ router.get("/:tresorId", isAuthenticated, (req, res, next) => {
 
 // update
 
-router.put("/:tresorId", (req, res, next) => {
+router.put("/:tresorId", isAuthenticated, (req, res, next) => {
   const { tresorId } = req.params;
-  const data = ({ title, messages } = req.body);
+  const { title, messages } = req.body;
+  const data = { title, messages };
   Tresor.findByIdAndUpdate(tresorId, data, { new: true })
     .then((response) => {
       //   console.log("response: ", response);
@@ -60,14 +88,23 @@ router.put("/:tresorId", (req, res, next) => {
 });
 // delete
 
-router.delete("/:tresorId", (req, res, next) => {
+router.delete("/:tresorId", isAuthenticated, (req, res, next) => {
   const { tresorId } = req.params;
-
-  Tresor.findByIdAndDelete(tresorId)
+  User.findByIdAndUpdate(
+    req.payload._id,
+    { $pull: { tresors: tresorId } },
+    { new: true }
+  )
     .then((response) => {
-      res.status(200).json(response);
+      user = response;
+      return Tresor.findByIdAndDelete(tresorId);
     })
-    .catch((err) => res.status(500).json(response));
+    .then((response) => {
+      res.status(200).json({
+        message: `deleted Tresor "${response.title}" with id: ${response.id}`,
+      });
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;

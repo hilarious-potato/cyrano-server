664be1d767fd948472c1eaba92b794e44dbcffb5
denefi <denefi@posteo.de>
2023-04-13 15:46:25 +0200

    Feat: Add authorization to routes (#5)
    * Add Middleware to check for admin status in User
    
    * add user role to jwt payload
    
    * add auth middleware to app.js routes
    
    * add user role to jwt payload
    
    * add user auth to tresor routes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+const { isAuthenticated } = require("./middleware/jwt.middleware");
+const isAdmin = require("./middleware/isAdmin");
 
 const app = express();
 
@@ -15,12 +17,16 @@ const app = express();
 require("./config")(app);
 
 // 👇 Start handling routes here
-
 app.use("/api", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/api/messages", require("./routes/message.routes"));
-app.use("/api/tresors", require("./routes/tresor.routes"));
-app.use("/api/reports", require("./routes/report.routes"));
+app.use("/api/tresors", isAuthenticated, require("./routes/tresor.routes"));
+app.use(
+  "/api/reports",
+  isAuthenticated,
+  isAdmin,
+  require("./routes/report.routes")
+);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);

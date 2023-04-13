e363a77db7a567cc087fa1b2f505d01ba6703e1a
denefi <denefi@posteo.de>
2023-04-13 18:23:24 +0200

    add admin auth to admin routes

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ require("./config")(app);
 app.use("/api", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/api/messages", require("./routes/message.routes"));
+app.use("/api/admin/reports", require("./routes/report.routes"));
 app.use(
   "/api/admin",
   isAuthenticated,
@@ -27,12 +28,6 @@ app.use(
   require("./routes/admin.routes")
 );
 app.use("/api/tresors", isAuthenticated, require("./routes/tresor.routes"));
-app.use(
-  "/api/reports",
-  isAuthenticated,
-  isAdmin,
-  require("./routes/report.routes")
-);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
